7ce0313cdd84d49d8c972a0b5d0b65d9d7fa9a42
moqi <moqimoqidea@gmail.com>
2020-12-11 17:06:39 +0800

    绘制 利萨如图形 V1

---
+++ ch01/a09_lissajous.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+	"time"
+)
+
+/*
+ * 绘制 利萨如图形 V1
+ *
+ * @author moqi
+ * On 12/11/20 16:56
+ */
+
+var palette = []color.Color{color.White, color.Black}
+
+const (
+	// whiteIndex = 0 // first color in palette
+	blackIndex = 0 // next color in palette
+)
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5
+		res     = 0.001
+		size    = 100
+		nFrames = 64
+		delay   = 8
+	)
+
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nFrames}
+	phase := 0.0
+
+	for i := 0; i < nFrames; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+		}
+
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+
+	_ = gif.EncodeAll(out, &anim)
+}
