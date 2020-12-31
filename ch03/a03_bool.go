f782070e6fd5156423dfe88b2226bb1633d34456
moqi <moqimoqidea@gmail.com>
2020-12-31 21:51:01 +0800

    update a03_bool.go

--- ch03/a03_bool.go
+++ ch03/a03_bool.go
@@ -1,11 +1,14 @@
 package main
 
 /**
+ * 布尔值
+ *
  * @author moqi
  * On 2020/12/31 21:41
  */
 func main() {
-	print(btoi(true))
+	println(btoi(true))
+	println(itob(100))
 }
 
 func btoi(b bool) int {
@@ -13,4 +16,8 @@ func btoi(b bool) int {
 		return 1
 	}
 	return 0
+}
+
+func itob(i int) bool {
+	return i != 0
 }
