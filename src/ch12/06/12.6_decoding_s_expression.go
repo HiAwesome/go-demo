c4080377f90ee8e2ea568b6a92e0114587aff3bd
moqi <moqimoqidea@gmail.com>
2021-01-24 21:32:23 +0800

    解码 S 表达式

---
+++ src/ch12/06/12.6_decoding_s_expression.go
@@ -0,0 +1,8 @@
+// 解码 S 表达式
+// @author moqi
+// On 2021/1/24 21:05:00
+package main
+
+func main() {
+
+}
---
+++ src/ch12/06/sexpr/sexpr_test.go
@@ -0,0 +1,45 @@
+//
+// @author moqi
+// On 2021/1/24 20:36:32
+package sexpr
+
+import (
+	sexpr04 "moqi.com/go/ch12/04/sexpr"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	type Movie struct {
+		Title, Subtitle string
+		Year            int
+		Actor           map[string]string
+		Oscars          []string
+		Sequel          *string
+	}
+	strangelove := Movie{
+		Title:    "Dr. Strangelove",
+		Subtitle: "How I Learned to Stop Worrying and Love the Bomb",
+		Year:     1964,
+		Actor: map[string]string{
+			"Dr. Strangelove":            "Peter Sellers",
+			"Grp. Capt. Lionel Mandrake": "Peter Sellers",
+			"Pres. Merkin Muffley":       "Peter Sellers",
+			"Gen. Buck Turgidson":        "George C. Scott",
+			"Brig. Gen. Jack D. Ripper":  "Sterling Hayden",
+			`Maj. T.J. "King" Kong`:      "Slim Pickens",
+		},
+		Oscars: []string{
+			"Best Actor (Nomin.)",
+			"Best Adapted Screenplay (Nomin.)",
+			"Best Director (Nomin.)",
+			"Best Picture (Nomin.)",
+		},
+	}
+
+	data, _ := sexpr04.Marshal(strangelove)
+	var movie Movie
+	if err := Unmarshal(data, &movie); err != nil {
+		t.Fatalf("unmarshal failed: %v", err)
+	}
+	t.Logf("Unmarshal() = %+v\n", movie)
+}
